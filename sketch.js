8dafb458ce8cbdd752c4a75b65e9f6cb123988f4
siddharth23052006 <siddharthshivakumar2@gmail.com>
2020-06-22 20:04:59 +0530

    made a multiplayer game with a ball

--- sketch.js
+++ sketch.js
@@ -1,29 +1,48 @@
 var ball;
+var database;
+var dbPosition;
 
 function setup(){
     createCanvas(500,500);
+    database = firebase.database();
+
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    var ballPositionRef = database.ref("ball/position");
+    ballPositionRef.on("value", readPosition, showError);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        'x': dbPosition.x + x,
+        'y': dbPosition.y + y
+    });
+}
+
+function readPosition(data){
+    dbPosition = data.val();
+    ball.x = dbPosition.x;
+    ball.y = dbPosition.y;
+}
+
+function showError(){
+    console.log("Error occured");
 }
